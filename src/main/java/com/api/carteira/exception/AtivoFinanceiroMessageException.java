75460ee08e4dc260a62aeb30ee653cce0674aade
DanubiaM <dubamacedoo@gmail.com>
2023-04-25 21:43:06 -0400

    feat: mensagens de expcetion

---
+++ src/main/java/com/api/carteira/exception/AtivoFinanceiroMessageException.java
@@ -0,0 +1,14 @@
+package com.api.carteira.exception;
+
+public class AtivoFinanceiroMessageException {
+    public static  final String TICKER_NAO_LOCALIZADO = "O Ticket não foi localizado";
+    public static  final String TICKER_JA_CADASTRADO = "O Ticket já esta cadastrado, por favor, insira um ticker diferente";
+    public static  final String TICKER_INVALIDO = "Ticker invalido, tente novamente";
+    public static  final String TIPO_INVALIDO = "Tipo invalido, tente novamente";
+    public static  final String PERCENTUAL_INVALIDO = "Percentual alocacao invalido, tente novamente";
+    public static  final String PRECO_INVALIDO = "Preco teto invalido, tente novamente";
+
+
+
+
+}
